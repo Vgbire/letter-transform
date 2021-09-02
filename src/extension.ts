f00cd41762d4ac2faacdcc6b2e9a74055e2e8e34
Vgbire <1053254202@qq.com>
2021-09-02 10:35:09 +0800

    feat: 获取选中的文本

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,20 @@
 import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('case-transform.helloWorld', (textEditor, edit) => { 
+  let disposable = vscode.commands.registerTextEditorCommand('case-transform.upper', (textEditor, edit) => {
+    textEditor.selections.forEach((item) => {
+      console.log(textEditor.document.getText(item))
+    })
     vscode.window.showInformationMessage('Hello World from case transform!')
   })
 
   context.subscriptions.push(disposable)
-	context.subscriptions.push(vscode.languages.registerSelectionRangeProvider('*', {
-		provideSelectionRanges: (document)=>{
-			console.log(document.getText())
-			return []
-		}
-	}
+  context.subscriptions.push(
+    vscode.languages.registerSelectionRangeProvider('*', {
+      provideSelectionRanges: (document) => {
+        console.log(document.getText())
+        return []
+      },
+    }),
+  )
 }
