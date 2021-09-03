a281e7fb55e2417d81cd876e3084b935b0e79776
Vgbire <1053254202@qq.com>
2021-09-03 19:48:38 +0800

    feat: add camelize feature

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,17 @@
 import * as vscode from 'vscode'
 
+const camelizeRE = /_(\w)/g
+export const camelize = (str: string): string => {
+  return str.replace(camelizeRE, (_, c) => (c ? c.toUpperCase() : ''))
+}
+
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerTextEditorCommand('case-transform.upper', (textEditor, edit) => {
     textEditor.selections.forEach((item) => {
-      console.log(textEditor.document.getText(item))
+      edit.replace(item, camelize(textEditor.document.getText(item)))
     })
     vscode.window.showInformationMessage('Hello World from case transform!')
   })
 
   context.subscriptions.push(disposable)
-  context.subscriptions.push(
-    vscode.languages.registerSelectionRangeProvider('*', {
-      provideSelectionRanges: (document) => {
-        console.log(document.getText())
-        return []
-      },
-    }),
-  )
 }
