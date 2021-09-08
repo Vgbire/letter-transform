02bbcab0f0844e4b1a65a8e13c0dd0fa2836d9b7
Vgbire <1053254202@qq.com>
2021-09-08 15:33:47 +0800

    feat: 完成所有功能

--- src/extension.ts
+++ src/extension.ts
@@ -5,13 +5,41 @@ export const camelize = (str: string): string => {
   return str.replace(camelizeRE, (_, c) => (c ? c.toUpperCase() : ''))
 }
 
-export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerTextEditorCommand('case-transform.upper', (textEditor, edit) => {
-    textEditor.selections.forEach((item) => {
-      edit.replace(item, camelize(textEditor.document.getText(item)))
-    })
-    vscode.window.showInformationMessage('Hello World from case transform!')
-  })
+const underlineRE = /\B([A-Z])/g
+export const underline = (str: string): string => {
+  return str.replace(underlineRE, '_$1').toLowerCase()
+}
 
-  context.subscriptions.push(disposable)
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerTextEditorCommand('case-transform.upper', (textEditor, edit) => {
+      textEditor.selections.forEach((item) => {
+        edit.replace(item, textEditor.document.getText(item).toUpperCase())
+      })
+    }),
+  )
+
+  context.subscriptions.push(
+    vscode.commands.registerTextEditorCommand('case-transform.lower', (textEditor, edit) => {
+      textEditor.selections.forEach((item) => {
+        edit.replace(item, textEditor.document.getText(item).toLowerCase())
+      })
+    }),
+  )
+
+  context.subscriptions.push(
+    vscode.commands.registerTextEditorCommand('case-transform.camel', (textEditor, edit) => {
+      textEditor.selections.forEach((item) => {
+        edit.replace(item, camelize(textEditor.document.getText(item)))
+      })
+    }),
+  )
+
+  context.subscriptions.push(
+    vscode.commands.registerTextEditorCommand('case-transform.underline', (textEditor, edit) => {
+      textEditor.selections.forEach((item) => {
+        edit.replace(item, underline(textEditor.document.getText(item)))
+      })
+    }),
+  )
 }
