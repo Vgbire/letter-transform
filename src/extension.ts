1f3869056e5d698ad1b5802a877a9254a8d3049d
vgbire <liwen.liao@ambergroup.io>
2022-03-07 19:52:35 +0800

    feat: 增加帕斯卡和短横线命名

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,28 @@
-import * as vscode from 'vscode'
+import { ExtensionContext, commands } from 'vscode'
 
 const camelizeRE = /[_-\s]+(\w)/g
 export const camelize = (str: string): string => {
   return str.replace(camelizeRE, (_, c) => (c ? c.toUpperCase() : ''))
 }
 
-const underlineRE = /\B([A-Z])/g
-export const underline = (str: string): string => {
-  // 先统一转成驼峰之后统一处理
-  let camelize = str.replace(camelizeRE, (_, c) => (c ? c.toUpperCase() : ''))
-  return camelize.replace(underlineRE, '_$1').toLowerCase().replace('/s/g', '_')
+export const pascal = (str: string): string => {
+  let result = str.replace(camelizeRE, (_, c) => (c ? c.toUpperCase() : ''))
+  result = result[0].toUpperCase() + result.slice(1)
+  return result
 }
 
-export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand('case-transform.upper', (textEditor, edit) => {
-      textEditor.selections.forEach((item) => {
-        edit.replace(item, textEditor.document.getText(item).toUpperCase())
-      })
-    }),
-  )
+const underlineRE = /\B([A-Z])/g
+export const addSeparator = (str: string, separator: string): string => {
+  // 先统一转成驼峰之后统一处理
+  return camelize(str)
+    .replace(underlineRE, separator + '$1')
+    .toLowerCase()
+    .replace('/s/g', '_')
+}
 
+export function activate(context: ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand('case-transform.lower', (textEditor, edit) => {
-      textEditor.selections.forEach((item) => {
-        edit.replace(item, textEditor.document.getText(item).toLowerCase())
-      })
-    }),
-  )
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand('case-transform.camel', (textEditor, edit) => {
+    commands.registerTextEditorCommand('case-transform.camel', (textEditor, edit) => {
       textEditor.selections.forEach((item) => {
         edit.replace(item, camelize(textEditor.document.getText(item)))
       })
@@ -38,9 +30,41 @@ export function activate(context: vscode.ExtensionContext) {
   )
 
   context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand('case-transform.underline', (textEditor, edit) => {
+    commands.registerTextEditorCommand('case-transform.underline', (textEditor, edit) => {
       textEditor.selections.forEach((item) => {
-        edit.replace(item, underline(textEditor.document.getText(item)))
+        edit.replace(item, addSeparator(textEditor.document.getText(item), '_'))
+      })
+    }),
+  )
+
+  context.subscriptions.push(
+    commands.registerTextEditorCommand('case-transform.kebab', (textEditor, edit) => {
+      textEditor.selections.forEach((item) => {
+        edit.replace(item, addSeparator(textEditor.document.getText(item), '-'))
+      })
+    }),
+  )
+
+  context.subscriptions.push(
+    commands.registerTextEditorCommand('case-transform.upper', (textEditor, edit) => {
+      textEditor.selections.forEach((item) => {
+        edit.replace(item, textEditor.document.getText(item).toUpperCase())
+      })
+    }),
+  )
+
+  context.subscriptions.push(
+    commands.registerTextEditorCommand('case-transform.lower', (textEditor, edit) => {
+      textEditor.selections.forEach((item) => {
+        edit.replace(item, textEditor.document.getText(item).toLowerCase())
+      })
+    }),
+  )
+
+  context.subscriptions.push(
+    commands.registerTextEditorCommand('case-transform.pascal', (textEditor, edit) => {
+      textEditor.selections.forEach((item) => {
+        edit.replace(item, pascal(textEditor.document.getText(item)))
       })
     }),
   )
